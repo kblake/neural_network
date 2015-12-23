56c3d0b3bdce07e2b27904965489d27bc8427061
Karmen Blake <dudeblake@gmail.com>
2015-12-23 11:48:38 -0800

    layer specs

--- lib/neural_network/layer.rb
+++ lib/neural_network/layer.rb
@@ -6,19 +6,19 @@ module NeuralNetwork
       @neurons = Array.new(size) { Neuron.new }
     end
 
-    def activate(values = nil)
-      values = Array(values) # coerce it to an array if nil
-
-      @neurons.each_with_index do |neuron, index|
-        neuron.activate(values[index])
-      end
-    end
-
     def connect(target_layer)
       @neurons.each do |source_neuron|
         target_layer.neurons.each do |target_neuron|
           source_neuron.connect(target_neuron)
         end
+      end
+    end
+
+    def activate(values = nil)
+      values = Array(values) # coerce it to an array if nil
+
+      @neurons.each_with_index do |neuron, index|
+        neuron.activate(values[index])
       end
     end
   end
--- spec/neural_network/layer_spec.rb
+++ spec/neural_network/layer_spec.rb
@@ -1,7 +1,63 @@
 require 'spec_helper'
 
 describe NeuralNetwork::Layer do
-  it 'does something useful' do
-    expect(false).to eq(true)
+  let(:layer) { described_class.new(2) }
+
+  it "initialize neurons" do
+    expect(layer.neurons.size).to eq 2
+    expect(layer.neurons.first.class).to eq NeuralNetwork::Neuron
+  end
+
+  context "connect layers" do
+    let(:input_layer)  { described_class.new(2) }
+    let(:output_layer) { described_class.new(2) }
+
+    it "not outgoing or incoming neurons connected" do
+      input_layer.neurons.each do |neuron|
+        expect(neuron.outgoing.size).to eq 0
+        expect(neuron.incoming.size).to eq 0
+      end
+
+      output_layer.neurons.each do |neuron|
+        expect(neuron.outgoing.size).to eq 0
+        expect(neuron.incoming.size).to eq 0
+      end
+    end
+
+    it "input layer's outgoing neurons are stored" do
+      input_layer.connect(output_layer)
+
+      input_layer.neurons.each do |neuron|
+        expect(neuron.outgoing.size).to eq 2
+        expect(neuron.outgoing.map(&:target)).to eq output_layer.neurons
+        expect(neuron.incoming.size).to eq 0
+      end
+    end
+
+    it "output layer's incoming neurons are stored" do
+      input_layer.connect(output_layer)
+
+      output_layer.neurons.each do |neuron|
+        expect(neuron.incoming.size).to eq 2
+        expect(neuron.incoming.map(&:source)).to eq input_layer.neurons
+        expect(neuron.outgoing.size).to eq 0
+      end
+    end
+  end
+
+  context "activating a layer" do
+    it "when values are nil" do
+      layer.activate
+      layer.neurons.each do |neuron|
+        expect(neuron.output).to eq 0.5
+      end
+    end
+
+    it "with values" do
+      layer.activate([1,2])
+      layer.neurons.each do |neuron|
+        expect(0.5..1).to cover(neuron.output)
+      end
+    end
   end
 end
